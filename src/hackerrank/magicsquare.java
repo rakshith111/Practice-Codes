53b3006f1fdb7945e27b0e7e00da988967a3ef22
rakshith111 <rakshithbabu111@gmail.com>
2021-10-03 16:56:46 +0530

    Solved

--- src/hackerrank/magicsquare.java
+++ src/hackerrank/magicsquare.java
@@ -3,6 +3,7 @@ package hackerrank;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Ref;
 import java.util.*;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -12,14 +13,17 @@ public class magicsquare {
     public  static int getmin(int ele, List<Integer> arr) {
         System.out.println("item got = "+ele);
         if(arr.size()==0) return 0;
-        int high=9,index=0;
+        int high=9,index=0,val=0;
         for(int item:arr){
             //System.out.println(" ele "+ele+" item "+item+" diff     "+Math.abs(item-ele));
             if(Math.abs(item-ele)<high){
+                val=item;
                 index=arr.indexOf((item));
                 high=Math.abs(item-ele);
             }
         };
+
+
         System.out.println("index "+index+" diff"+high);
         arr.remove(index);
         System.out.println(arr);
@@ -50,46 +54,9 @@ public class magicsquare {
 //        3 3 4
 //        4 5 6
 //        1 2 3
-
-        List<Integer> flat_array = new ArrayList<>();//[3, 3, 4, 4, 5, 6, 1, 2, 3]
-        flat_array.add(0);
-        s.forEach(flat_array::addAll);
-        int minimal = 0, val;
-
-        for (int i = 1; i < flat_array.size(); i++) {
-            val = flat_array.get(i);
-            if (i != 5) {
-                if (i % 2 == 0) {
-                    if (val % 2 != 0) {
-                        try {
-                            Reference.odd.remove(Reference.odd.indexOf(val));
-                        } catch (Exception e) {
-                        }
-                    } else {
-                        System.out.println("pos " + i + " val " + val);
-                        minimal += getmin(val, Reference.odd);
-                    }
-
-                } else {
-                    if (val % 2 == 0) {
-                        try {
-                            Reference.even.remove(Reference.even.indexOf(val));
-                        } catch (Exception e) {
-                        }
-                    } else {
-                        System.out.println("pos " + i + " val " + val);
-                        minimal += getmin(val, Reference.even);
-
-                    }
-
-                }
-
-            }else{
-                minimal+=Math.abs(val-5);
-            }
-        }
-        System.out.println("min"+minimal);
+    s.forEach(i ->{
+        System.out.println(i);
+    });
 
     }
 }
-
